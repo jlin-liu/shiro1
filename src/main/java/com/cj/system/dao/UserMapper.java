1023097e0b0cfeff37878572a1892262b442d3f1
jlin-liu <ljlin19960811@163.com>
2020-07-09 16:51:00 +0800

    test

--- src/main/java/com/cj/system/dao/UserMapper.java
+++ src/main/java/com/cj/system/dao/UserMapper.java
@@ -3,8 +3,12 @@ package com.cj.system.dao;
 import com.cj.system.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
 
     User selectByName(String username);
+
+    List<User> selectUser();
 }
